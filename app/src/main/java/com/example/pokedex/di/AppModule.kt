d4814aa2bd1faa88c17b3f29293dd3cf43350a23
pranavv <pranavvyas2003@gmail.com>
2022-12-22 00:14:44 +0530

    feat: setup room db

--- app/src/main/java/com/example/pokedex/di/AppModule.kt
+++ app/src/main/java/com/example/pokedex/di/AppModule.kt
@@ -1,12 +1,15 @@
 package com.example.pokedex.di
 
-import androidx.compose.ui.unit.Constraints
+import android.content.Context
+import com.example.pokedex.db.PokeDao
+import com.example.pokedex.db.PokemonDatabase
 import com.example.pokedex.remote.PokeAPi
 import com.example.pokedex.repository.PokeRepository
 import com.example.pokedex.util.Constants
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -17,17 +20,27 @@ import javax.inject.Singleton
 object AppModule {
     @Provides
     @Singleton
-    fun providePokeApi(): PokeAPi{
+    fun providePokeApi(): PokeAPi {
         return Retrofit.Builder()
             .baseUrl(Constants.BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(PokeAPi::class.java)
     }
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext context: Context): PokemonDatabase =
+        PokemonDatabase.create(context)
 
     @Provides
     @Singleton
-     fun providePokeRepository(pokeApi: PokeAPi): PokeRepository{
-        return PokeRepository(pokeApi)
+    fun provideDao(database: PokemonDatabase): PokeDao {
+        return database.getPokeDao()
+    }
+
+    @Provides
+    @Singleton
+    fun providePokeRepository(pokeApi: PokeAPi, pokeDao: PokeDao): PokeRepository {
+        return PokeRepository(pokeApi, pokeDao)
     }
 }
