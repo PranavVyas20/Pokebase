6a92c8acb54ed9ec908775f7848925c1003cafcc
pranavv <pranavvyas2003@gmail.com>
2022-12-22 02:27:48 +0530

    fix: pokelist card height

--- app/src/main/java/com/example/pokedex/ui_components/PokeListItem.kt
+++ app/src/main/java/com/example/pokedex/ui_components/PokeListItem.kt
@@ -1,10 +1,11 @@
 package com.example.pokedex.ui_components
 
 import android.graphics.drawable.Drawable
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
@@ -16,6 +17,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
@@ -41,22 +43,27 @@ fun PokeListItem(
     navController: NavController?
 ) {
     val showShimmer = remember { mutableStateOf(true) }
-    val pokemonDominantColor = remember { mutableStateOf(Color.White)}
+    val pokemonDominantColor = remember { mutableStateOf(Color.White) }
 
     val loadedPokemonDrawable = remember {
         mutableStateOf<Drawable?>(null)
     }
 
+    val imgLoaded = remember {
+        mutableStateOf(false)
+    }
     Card(
         modifier = Modifier
             .padding(8.dp)
-            .defaultMinSize(40.dp, 60.dp)
             .clickable {
                 // Make api call
                 if (!showShimmer.value) {
 //                    getPokemonDetails(pokemonName.replaceFirstChar { it.lowercaseChar() }, true)
-                    if(getDominantColorAndDrawable != null) {
-                        getDominantColorAndDrawable(pokemonDominantColor.value,loadedPokemonDrawable.value)
+                    if (getDominantColorAndDrawable != null) {
+                        getDominantColorAndDrawable(
+                            pokemonDominantColor.value,
+                            loadedPokemonDrawable.value
+                        )
                     }
                     navController?.navigate(Screen.PokemonDetailScreen.route + "/$pokemonName")
                 }
@@ -72,24 +79,38 @@ fun PokeListItem(
         backgroundColor = pokemonDominantColor.value,
         shape = RoundedCornerShape(15.dp)
     ) {
-        Column(modifier = Modifier.padding(20.dp), verticalArrangement = Arrangement.spacedBy(10.dp)) {
-            AsyncImage(
-                model = ImageRequest.Builder(LocalContext.current)
-                    .data(pokemonImageUrl)
-                    .build(),
-                onSuccess = {
-                    if(calculateDominantColor != null) {
-                        calculateDominantColor(it.result.drawable) { color ->
-                            pokemonDominantColor.value = color
-                            loadedPokemonDrawable.value = it.result.drawable
+        Column(
+            modifier = Modifier.padding(20.dp),
+            verticalArrangement = Arrangement.spacedBy(10.dp)
+        ) {
+            Box() {
+                if (!imgLoaded.value) {
+                    Image(
+                        painter = painterResource(id = R.drawable.pokeball_bg),
+                        contentDescription = ""
+                    )
+                }
+                AsyncImage(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(pokemonImageUrl)
+                        .build(),
+                    placeholder = painterResource(id = R.drawable.pokeball_bg),
+                    onSuccess = {
+                        imgLoaded.value = true
+                        if (calculateDominantColor != null) {
+                            calculateDominantColor(it.result.drawable) { color ->
+                                pokemonDominantColor.value = color
+                                loadedPokemonDrawable.value = it.result.drawable
+                            }
                         }
-                    }
-                    showShimmer.value = false
-                },
-                onLoading = {
-                },
-                contentDescription = "",
-            )
+                        showShimmer.value = false
+                    },
+                    onLoading = {
+                    },
+                    contentDescription = "",
+                )
+            }
+
             Column(
                 modifier = Modifier.align(Alignment.CenterHorizontally),
                 horizontalAlignment = Alignment.CenterHorizontally
@@ -100,8 +121,10 @@ fun PokeListItem(
                     color = Color(0xFF442a60)
                 )
                 Text(
-                    text = pokemonFormattedNumber, color = Color(0xFF442a60)
-                )
+                    text = pokemonFormattedNumber,
+                    color = Color(0xFF442a60),
+                    fontFamily = FontFamily(Font(R.font.tt_interfaces_bold)),
+                    )
             }
         }
     }
